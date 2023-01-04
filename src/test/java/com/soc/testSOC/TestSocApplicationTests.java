c3aad5475254b16bb52dadb73be8e111a5812bae
Matheus Vieira <matheusvieira.contato@hotmail.com>
2023-01-04 16:13:37 -0300

    Project Created

---
+++ src/test/java/com/soc/testSOC/TestSocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soc.testSOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
