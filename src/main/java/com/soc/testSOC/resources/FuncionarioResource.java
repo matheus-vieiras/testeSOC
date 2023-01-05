e494cd0ce61a3df38c0d148ee2e8a4ee0919498e
Matheus Vieira <matheusvieira.contato@hotmail.com>
2023-01-05 13:56:05 -0300

    Service layer, component registration

--- src/main/java/com/soc/testSOC/resources/FuncionarioResource.java
+++ src/main/java/com/soc/testSOC/resources/FuncionarioResource.java
@@ -1,18 +1,32 @@
 package com.soc.testSOC.resources;
 
 import com.soc.testSOC.entities.Funcionario;
+import com.soc.testSOC.services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/funcionarios")
 public class FuncionarioResource {
 
+    @Autowired
+    private FuncionarioService service;
+
     @GetMapping
-    public ResponseEntity<Funcionario> findAll() {
-        Funcionario f = new Funcionario(1L, "Maria");
-        return ResponseEntity.ok().body(f);
+    public ResponseEntity<List<Funcionario>> findAll() {
+        List<Funcionario> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Funcionario> findById(@PathVariable Long id) {
+        Funcionario obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/soc/testSOC/services/FuncionarioService.java
@@ -0,0 +1,25 @@
+package com.soc.testSOC.services;
+
+import com.soc.testSOC.entities.Funcionario;
+import com.soc.testSOC.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository repository;
+
+    public List<Funcionario> findAll() {
+        return repository.findAll();
+    }
+
+    public Funcionario findById(Long id) {
+        Optional<Funcionario> obj = repository.findById(id);
+        return obj.get();
+    }
+}
